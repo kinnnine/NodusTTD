ca94b9fcf0ebdfb9e3ad7247fb113620661ee1ce
kinnnine <nakaharaml9@gmail.com>
2026-09-21 04:27:31 +0700

    scripts: build script testing

--- scripts/build_openttd.sh
+++ scripts/build_openttd.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+# https://github.com/OpenTTD/OpenTTD/blob/15.3/os/emscripten/README.md
+
+cd OpenTTD_base
+
+(
+	cd os/emscripten
+	docker build -t emsdk-openttd .
+)
+
+mkdir build-host
+docker run -it --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) --workdir $(pwd)/build-host emsdk-openttd cmake .. -DOPTION_TOOLS_ONLY=ON
+docker run -it --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) --workdir $(pwd)/build-host emsdk-openttd make -j$(nproc) tools
+
+mkdir build
+docker run -it --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) --workdir $(pwd)/build emsdk-openttd emcmake cmake .. -DHOST_BINARY_DIR=../build-host -DCMAKE_BUILD_TYPE=Release -DOPTION_USE_ASSERTS=OFF
+docker run -it --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) --workdir $(pwd)/build emsdk-openttd emmake make -j$(nproc)
+
+ls -lha build
