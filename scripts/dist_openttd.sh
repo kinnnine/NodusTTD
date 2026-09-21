3938708ac2a278fbf69986747b6db90bc5203f49
kinnnine <nakaharaml9@gmail.com>
2026-09-21 14:35:11 +0700

    scripts: typo on dist script

--- scripts/dist_openttd.sh
+++ scripts/dist_openttd.sh
@@ -10,7 +10,7 @@ cp -v OpenTTD_base/build/openttd.js			dist/openttd.js
 cp -v OpenTTD_base/build/openttd.wasm		dist/openttd.wasm
 cp -v OpenTTD_base/build/openttd.html		dist/index.html
 
-cp -v public/manifest.json					dist/mainfest.json
+cp -v public/manifest.json					dist/manifest.json
 cp -v public/sw.js 							dist/sw.js
 
 cp -v OpenTTD_base/media/openttd.512.png	dist/logo512.png
