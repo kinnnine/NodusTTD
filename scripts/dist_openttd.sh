a8e8022466c3b1a43515d96ea4ba9a2e1da80c8c
kinnnine <nakaharaml9@gmail.com>
2026-09-21 08:44:29 +0700

    public: added manifest.json and sw.js

--- scripts/dist_openttd.sh
+++ scripts/dist_openttd.sh
@@ -5,7 +5,12 @@
 
 rm -rf dist && mkdir dist
 
-cp -v OpenTTD_base/build/openttd.data	dist/openttd.data
-cp -v OpenTTD_base/build/openttd.js		dist/openttd.js
-cp -v OpenTTD_base/build/openttd.wasm	dist/openttd.wasm
-cp -v OpenTTD_base/build/openttd.html	dist/index.html
+cp -v OpenTTD_base/build/openttd.data		dist/openttd.data
+cp -v OpenTTD_base/build/openttd.js			dist/openttd.js
+cp -v OpenTTD_base/build/openttd.wasm		dist/openttd.wasm
+cp -v OpenTTD_base/build/openttd.html		dist/index.html
+
+cp -v public/manifest.json					dist/mainfest.json
+cp -v public/sw.js 							dist/sw.js
+
+cp -v OpenTTD_work/media/openttd.128.png	dist/favicon.png
