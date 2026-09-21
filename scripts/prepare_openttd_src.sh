6f4d1e35b8b802921b0f12ff10ac44f9d39dc535
kinnnine <nakaharaml9@gmail.com>
2026-09-21 08:16:57 +0700

    scripts: preparing keep .git .gtihub now

--- scripts/prepare_openttd_src.sh
+++ scripts/prepare_openttd_src.sh
@@ -14,19 +14,4 @@ fi
 
 git clone "$OPENTTD_REPOURL" --branch "$OPENTTD_VERSION" --depth 1 OpenTTD_base
 
-# remove huge folders for size
-rm -rf OpenTTD_base/.git OpenTTD_base/.github
-
-# truncate any icon and logo files for size
-: > OpenTTD_base/media/openttd.svg
-for file in OpenTTD_base/media/*.png; do
-	: > "$file"
-done
-for file in OpenTTD_base/media/*.xpm; do
-	: > "$file"
-done
-for file in OpenTTD_base/media/*.bmp; do
-	: > "$file"
-done
-
 tar -czf openttd.tar.gz -C OpenTTD_base .
