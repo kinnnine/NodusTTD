26373f528b4730e58f729e5c072b4106f579f1df
kinnnine <nakaharaml9@gmail.com>
2026-09-22 11:23:57 +0700

    scripts: ability to create patchfile even source file not exist

--- scripts/make_patches.sh
+++ scripts/make_patches.sh
@@ -4,13 +4,18 @@ PATCHLIST="patches/patchlist.txt"
 
 rm -rf patches && mkdir patches
 
-diff -rqa OpenTTD_base/ OpenTTD_work/ | awk '{print $2}' > "$PATCHLIST"
+diff -Nrqa OpenTTD_base/ OpenTTD_work/ | \
+	awk '{print $2}' > "$PATCHLIST"
+
+diff -rqa OpenTTD_base/ OpenTTD_work/ | \
+	awk '/^Only in / {sub(/:/, "/"); sub(/OpenTTD_work/, "OpenTTD_base"); print $3 $4}'  >> "$PATCHLIST"
 
 while IFS= read -r line
 do
 	FILEPATH="${line/'OpenTTD_base/'/}"
 	PARENTPATH="${FILEPATH%/*}"
 	mkdir -p patches/"$PARENTPATH"
-	diff -Naru "$line" OpenTTD_work/"$FILEPATH" | sed -re '1,2 s/\t.*//' > patches/"$FILEPATH".patch
-	echo "$line Done."
+	[ ! -f "$line" ] && line="/dev/null" # if the file inside OpenTTD_base doesn't exist
+	diff -u "$line" OpenTTD_work/"$FILEPATH" | sed -re '1,2 s/\t.*//' > patches/"$FILEPATH".patch
+	echo "$FILEPATH Done."
 done < "$PATCHLIST"
