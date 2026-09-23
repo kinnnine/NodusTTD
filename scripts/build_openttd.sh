4c7726a4c1072d45526ffa5dd51076c7eb59ebe1
kinnnine <nakaharaml9@gmail.com>
2026-09-24 02:01:20 +0700

    src: added NodusTTD version string

--- scripts/build_openttd.sh
+++ scripts/build_openttd.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # https://github.com/OpenTTD/OpenTTD/blob/15.3/os/emscripten/README.md
 
+NODUSTTD_TAG="$(git tag --points-at HEAD)"
+if [ -z "$NODUSTTD_TAG" ]; then
+	NODUSTTD_TAG="$(git rev-parse --short HEAD)"
+fi
+
 (
 	cd OpenTTD_base/os/emscripten
 	docker build -t emsdk-openttd .
@@ -17,7 +22,8 @@
 	mkdir build
 	docker run -i --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) \
 		--workdir $(pwd)/build emsdk-openttd emcmake cmake .. \
-		-DHOST_BINARY_DIR=../build-host -DCMAKE_BUILD_TYPE=Release -DOPTION_USE_ASSERTS=OFF
+		-DHOST_BINARY_DIR=../build-host -DCMAKE_BUILD_TYPE=Release \
+		-DOPTION_USE_ASSERTS=OFF -DREV_NODUSTTD_VERSION=$NODUSTTD_TAG
 	docker run -i --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) \
 		--workdir $(pwd)/build emsdk-openttd emmake make -j$(nproc)
 )
