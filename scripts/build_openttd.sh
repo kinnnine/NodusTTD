238493e64fd8edbbfc230fe3c5e7449bbd61ab68
kinnnine <nakaharaml9@gmail.com>
2026-09-24 13:40:13 +0700

    cmake/scripts: check and apply NodusTTD version

--- scripts/build_openttd.sh
+++ scripts/build_openttd.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # https://github.com/OpenTTD/OpenTTD/blob/15.3/os/emscripten/README.md
 
-NODUSTTD_TAG="$(git tag --points-at HEAD)"
-if [ -z "$NODUSTTD_TAG" ]; then
-	NODUSTTD_TAG="$(git rev-parse --short HEAD)"
-fi
-
 (
 	cd OpenTTD_base/os/emscripten
 	docker build -t emsdk-openttd .
@@ -17,13 +12,13 @@ fi
 	docker run -i --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) \
 		--workdir $(pwd)/build-host emsdk-openttd cmake .. -DOPTION_TOOLS_ONLY=ON
 	docker run -i --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) \
-		--workdir $(pwd)/build-host emsdk-openttd make -j$(nproc) tools
+		--workdir $(pwd)/build-host \
+		emsdk-openttd make -j$(nproc) tools
 
 	mkdir build
 	docker run -i --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) \
 		--workdir $(pwd)/build emsdk-openttd emcmake cmake .. \
-		-DHOST_BINARY_DIR=../build-host -DCMAKE_BUILD_TYPE=Release \
-		-DOPTION_USE_ASSERTS=OFF -DREV_NODUSTTD_VERSION=$NODUSTTD_TAG
+		-DHOST_BINARY_DIR=../build-host -DCMAKE_BUILD_TYPE=Release -DOPTION_USE_ASSERTS=OFF
 	docker run -i --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) \
 		--workdir $(pwd)/build emsdk-openttd emmake make -j$(nproc)
 )
