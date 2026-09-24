190b647c8753bd9b75c28391e07626692f607bb2
kinnnine <nakaharaml9@gmail.com>
2026-09-24 14:59:26 +0700

    scripts: workdir at NodusTTD root folder
    fixes CMake won't see any folders/files other than OpenTTD_base, being set as workdir

--- scripts/build_openttd.sh
+++ scripts/build_openttd.sh
@@ -7,18 +7,17 @@
 )
 
 (
-	cd OpenTTD_base
-	mkdir build-host
+	mkdir OpenTTD_base/build-host
 	docker run -i --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) \
-		--workdir $(pwd)/build-host emsdk-openttd cmake .. -DOPTION_TOOLS_ONLY=ON
+		--workdir $(pwd)/OpenTTD_base/build-host emsdk-openttd cmake .. -DOPTION_TOOLS_ONLY=ON
 	docker run -i --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) \
-		--workdir $(pwd)/build-host \
+		--workdir $(pwd)/OpenTTD_base/build-host \
 		emsdk-openttd make -j$(nproc) tools
 
-	mkdir build
+	mkdir OpenTTD_base/build
 	docker run -i --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) \
-		--workdir $(pwd)/build emsdk-openttd emcmake cmake .. \
+		--workdir $(pwd)/OpenTTD_base/build emsdk-openttd emcmake cmake .. \
 		-DHOST_BINARY_DIR=../build-host -DCMAKE_BUILD_TYPE=Release -DOPTION_USE_ASSERTS=OFF
 	docker run -i --rm -v $(pwd):$(pwd) -u $(id -u):$(id -g) \
-		--workdir $(pwd)/build emsdk-openttd emmake make -j$(nproc)
+		--workdir $(pwd)/OpenTTD_base/build emsdk-openttd emmake make -j$(nproc)
 )
